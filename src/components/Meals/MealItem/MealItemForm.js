7261a2fa02df5e7ffb800821383b26aa70ca2261
fengcheng0126 <fengcheng0126@gmail.com>
2022-07-25 16:03:49 -0500

    meal form

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,12 @@
+import classes from "./MealItemForm.module.css";
+
+const MealItemForm = (props) => {
+  return (
+    <form classesName={classes.form}>
+      <input />
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
